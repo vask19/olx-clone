2b831523100ea525033cd885bc46ff9750d83f3a
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-10-13 13:19:30 +0200

    heroku

---
+++ src/main/java/com/vask/ysellbtoheroku/YsellbToHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.vask.ysellbtoheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsellbToHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YsellbToHerokuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vask/ysellbtoheroku/YsellbToHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vask.ysellbtoheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsellbToHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
