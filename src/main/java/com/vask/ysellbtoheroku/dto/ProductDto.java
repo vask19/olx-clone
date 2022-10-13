fcc66969e58f472d77e89209718e19580a00effb
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-10-13 21:09:02 +0200

    heroku

--- src/main/java/com/vask/ysellbtoheroku/dto/ProductDto.java
+++ src/main/java/com/vask/ysellbtoheroku/dto/ProductDto.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +28,7 @@ public class ProductDto {
     private Integer userId;
     private String ownerUsername;
     private Long previewImageId;
+    private LocalDateTime dateOfCreated;
 
     public ProductDto(Integer bookId) {
         this.id = bookId;
