61c8f921253a179ee0b6e4bfd93cb5e4e58c6076
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-10-13 15:15:39 +0200

    heroku

---
+++ src/main/resources/db/migration/V2__add_admin.sql
@@ -0,0 +1,3 @@
+insert into users (active, active_email, avatar_id, date_of_created, email, email_activation_code, first_name, password, phone_number, role, second_name, username)
+values
+    (true, true, null, null, 'ysellby@gmail.com', 11111, 'Vasyl', '$2a$10$aR6Geq0Vr44J80We0zmIc.QlOLUWleDskyYnBAgCO5U2ELw5yduoG', '+48731324466', 'ROLE_ADMIN', 'Korol', 'vask')
